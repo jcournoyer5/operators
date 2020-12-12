75f303c11118a5579da9dcfb6233f99da0f9d7e5
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-12 20:51:23 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,17 +8,8 @@ def unsafe?(speed)
    end
 
 
-    
-def not_safe?(speed)    
-  if  speed < 40 
-      return true
-  elsif speed > 60 
-        return true
-  else speed > 40 && speed < 60 ? false : true
-	
-  end
 
-	
-
+def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 end
 end
